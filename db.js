29737175a62a64d2bc26068d080a060384823a94
RbCaVi <robert@robertvail.info>
2024-10-14 18:42:06 -0400

    rename the functions in db and fix an injection "vulnerability"

--- db.js
+++ db.js
@@ -1,7 +1,7 @@
 const url = 'https://qryngvppzavugzdeheuk.supabase.co/rest/v1';
 const apikey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InFyeW5ndnBwemF2dWd6ZGVoZXVrIiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjc1NjUzMzEsImV4cCI6MjA0MzE0MTMzMX0.1H1MWNCfd0cro4TBk4ak529iMPlpN8RahITQuvCrXR0';
 
-function calldb(func, data) {
+function db_call(func, data) {
   return fetch(`${url}/rpc/${func}`, {
 		method: "POST",
 		body: data,
@@ -12,9 +12,8 @@ function calldb(func, data) {
 	});
 }
 
-function selectdb(table, params) {
-  const parts = params.entries().map(([k,v]) => `${k}=${v}`);
-  const query = parts.join('&');
+function db_select(table, params) {
+  const query = new URLSearchParams(params).toString();
   return fetch(`${url}/${table}?${query}`, {
 		headers: {
 			apikey: apikey,
