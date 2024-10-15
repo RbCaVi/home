308e5b8cfe693046a763ac605a31d92bb8854f65
RbCaViTerminal <robert@robertvail.info>
2024-10-15 18:16:30 -0400

    hash returns a hex string

--- hash.js
+++ hash.js
@@ -17,5 +17,5 @@ const hash = function(str, seed = 0) {
   h1 ^= Math.imul(h1 ^ (h2 >>> 15), 0x735a2d97);
   h2 ^= Math.imul(h2 ^ (h1 >>> 15), 0xcaf649a9);
   h1 ^= h2 >>> 16; h2 ^= h1 >>> 16;
-  return 2097152 * (h2 >>> 0) + (h1 >>> 11);
+  return h2.toString(16) + h1.toString(16);
 };
