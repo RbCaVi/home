1b18cd48acd2c09e6e37b078bd4f0776e55a5d78
RbCaViTerminal <robert@robertvail.info>
2024-11-01 08:28:25 -0400

    sort messages on the server + newer messages at the top

--- message.js
+++ message.js
@@ -5,10 +5,8 @@ function sendmessage(token, chatid, message) {
 async function seemessages(chatid) {
   return (await db_select(
     'textmessages',
-    {chat: `eq.${chatid}`, select: "created_at,content,users(username)"}
-  )).sort(({created_at1}, {created_at2}) => {
-    return Date(created_at1) - Date(created_at2);
-  }).map(({created_at, content, users: {username}}) => {
+    {chat: `eq.${chatid}`, select: "created_at,content,users(username)", order: "created_at.desc"}
+  )).map(({created_at, content, users: {username}}) => {
     return {
       timestamp: created_at,
       message: content,
