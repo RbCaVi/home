df17cb81eb7d3f8511edd941bbb5e7ba8e875d6b
RbCaVi <robert@vail-family.com>
2025-03-09 23:21:48 -0400

    add a static generator
    extra crunchy

---
+++ regen.py
@@ -0,0 +1,16 @@
+import generators
+
+generated = {}
+
+for path in generators.generatedfiles():
+    data = generators.getgenerator(path)(path) # i'm <age> and this is aeh
+    if type(data) == str:
+        data = bytes(data, 'utf-8')
+    generated[path] = data
+
+for path in generators.hiddenfiles():
+    os.remove(path)
+
+for path,data in generated.items():
+    with open(path, 'wb') as f:
+        f.write(data)
