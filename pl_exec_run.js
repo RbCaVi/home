d7c066eda334a17e54f34ef6134893109e36a2d1
RbCaViTerminal <robert@robertvail.info>
2025-03-19 16:41:50 -0400

    // no. no utf 8.

--- pl_exec_run.js
+++ pl_exec_run.js
@@ -17,37 +17,10 @@ function abort(what) {
   throw new WebAssembly.RuntimeError(what);
 }
 
-var UTF8Decoder = typeof TextDecoder != 'undefined' ? new TextDecoder('utf8') : undefined;
-
-var UTF8ArrayToString = (heapOrArray, idx, maxBytesToRead) => {
-  var endIdx = idx + maxBytesToRead;
-  var endPtr = idx;
-
-  while (heapOrArray[endPtr] && !(endPtr >= endIdx)) ++endPtr;
-
-  if (endPtr - idx > 16 && heapOrArray.buffer && UTF8Decoder) {
-    return UTF8Decoder.decode(heapOrArray.subarray(idx, endPtr));
-  }
+var UTF8ArrayToString = (heapOrArray, idx) => {
   var str = '';
-  while (idx < endPtr) {
-    var u0 = heapOrArray[idx++];
-    if (!(u0 & 0x80)) { str += String.fromCharCode(u0); continue; }
-    var u1 = heapOrArray[idx++] & 63;
-    if ((u0 & 0xE0) == 0xC0) { str += String.fromCharCode(((u0 & 31) << 6) | u1); continue; }
-    var u2 = heapOrArray[idx++] & 63;
-    if ((u0 & 0xF0) == 0xE0) {
-      u0 = ((u0 & 15) << 12) | (u1 << 6) | u2;
-    } else {
-      if ((u0 & 0xF8) != 0xF0) err('Invalid UTF-8 leading byte ' + '0x' + (u0 >>>= 0).toString(16).padStart(8, '0') + ' encountered when deserializing a UTF-8 string in wasm memory to a JS string!');
-      u0 = ((u0 & 7) << 18) | (u1 << 12) | (u2 << 6) | (heapOrArray[idx++] & 63);
-    }
-
-    if (u0 < 0x10000) {
-      str += String.fromCharCode(u0);
-    } else {
-      var ch = u0 - 0x10000;
-      str += String.fromCharCode(0xD800 | (ch >> 10), 0xDC00 | (ch & 0x3FF));
-    }
+  while (heapOrArray[idx]) {
+    str += String.fromCharCode(heapOrArray[idx++]); // no. no utf 8.
   }
   return str;
 };
