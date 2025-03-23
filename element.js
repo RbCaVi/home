e8819166e3a2af9c26f012f4c2f73998af999050
RbCaVi <robert@vail-family.com>
2025-03-23 18:19:13 -0400

    factor out element() from desktop.html

---
+++ element.js
@@ -0,0 +1,33 @@
+function element(type, attrs = {}, ...parts) {
+  const e = document.createElement(type);
+
+  if ("classes" in attrs) {
+    for (const c of attrs.classes) {
+      e.classList.add(c);
+    }
+  }
+
+  if ("id" in attrs) {
+    e.id = attrs.id;
+  }
+
+  if ("events" in attrs) {
+    for (const event in attrs.events) {
+      e.addEventListener(event, attrs.events[event]);
+    }
+  }
+
+  if ("style" in attrs) {
+    e.style = attrs.style;
+  }
+
+  if ("type" in attrs) {
+    e.type = attrs.type;
+  }
+
+  for (const part of parts) {
+    e.append(part);
+  }
+
+  return e;
+}
