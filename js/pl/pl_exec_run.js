8862787e1b99838f09efdfebd2bfd6f925075dc9
RbCaVi <robert@vail-family.com>
2025-03-23 22:34:53 -0400

    reinitialize the wasm context if the code aborts
    does not protect against infinite loops though :/

--- js/pl/pl_exec_run.js
+++ js/pl/pl_exec_run.js
@@ -12,12 +12,11 @@ var HEAP8, HEAPU8, HEAPU32;
 function abort(what) {
   what = 'Aborted(' + what + ')';
   printError(what);
+  
+  promiseInitialized = initialize();
 
   throw new WebAssembly.RuntimeError(what);
 }
-
-let resolveInitialized;
-const promiseInitialized = new Promise(res => resolveInitialized = res);
 
 var UTF8Decoder = typeof TextDecoder != 'undefined' ? new TextDecoder('utf8') : undefined;
 
@@ -68,54 +67,53 @@ var UTF8ToString = (ptr, maxBytesToRead) => {
   return ptr ? UTF8ArrayToString(HEAPU8, ptr, maxBytesToRead) : '';
 };
 
-fetch('pl_exec_run.wasm').then((response) => {
-  return WebAssembly.instantiateStreaming(response, {
-    'env': {
-      __assert_fail: (condition, filename, line, func) => {
-        abort(`Assertion failed: ${UTF8ToString(condition)}, at: ` + [filename ? UTF8ToString(filename) : 'unknown filename', line, func ? UTF8ToString(func) : 'unknown function']);
-      },
-      abort: () => abort('native code called abort()'),
-      emscripten_memcpy_big: (dest, src, num) => HEAPU8.copyWithin(dest, src, src + num),
-      emscripten_resize_heap: (requestedSize) => abort(`Cannot enlarge memory (OOM).`),
-    },
-    'wasi_snapshot_preview1': {
-      fd_write: (fd, iov, iovcnt, pnum) => {
-        var printCharBuffers = [null,[],[]];
-          
-        var printChar = (stream, curr) => {
-          var buffer = printCharBuffers[stream];
-          if (curr === 0 || curr === 10) {
-            (stream === 1 ? print : printError)(UTF8ArrayToString(buffer, 0));
-            buffer.length = 0;
-          } else {
-            buffer.push(curr);
-          }
-        };
 
-        var num = 0;
-        for (var i = 0; i < iovcnt; i++) {
-          var ptr = HEAPU32[((iov)>>2)];
-          var len = HEAPU32[(((iov)+(4))>>2)];
-          iov += 8;
-          for (var j = 0; j < len; j++) {
-            printChar(fd, HEAPU8[ptr+j]);
-          }
-          num += len;
+initialize = () => fetch('pl_exec_run.wasm').then(response => WebAssembly.instantiateStreaming(response, {
+  'env': {
+    __assert_fail: (condition, filename, line, func) => {
+      abort(`Assertion failed: ${UTF8ToString(condition)}, at: ` + [filename ? UTF8ToString(filename) : 'unknown filename', line, func ? UTF8ToString(func) : 'unknown function']);
+    },
+    abort: () => abort('native code called abort()'),
+    emscripten_memcpy_big: (dest, src, num) => HEAPU8.copyWithin(dest, src, src + num),
+    emscripten_resize_heap: (requestedSize) => abort(`Cannot enlarge memory (OOM).`),
+  },
+  'wasi_snapshot_preview1': {
+    fd_write: (fd, iov, iovcnt, pnum) => {
+      var printCharBuffers = [null,[],[]];
+        
+      var printChar = (stream, curr) => {
+        var buffer = printCharBuffers[stream];
+        if (curr === 0 || curr === 10) {
+          (stream === 1 ? print : printError)(UTF8ArrayToString(buffer, 0));
+          buffer.length = 0;
+        } else {
+          buffer.push(curr);
         }
-        HEAPU32[((pnum)>>2)] = num;
-        return 0;
+      };
+
+      var num = 0;
+      for (var i = 0; i < iovcnt; i++) {
+        var ptr = HEAPU32[((iov)>>2)];
+        var len = HEAPU32[(((iov)+(4))>>2)];
+        iov += 8;
+        for (var j = 0; j < len; j++) {
+          printChar(fd, HEAPU8[ptr+j]);
+        }
+        num += len;
       }
-    },
-  }).then(function receiveInstantiationResult(result) {
-    wasmExports = result.instance.exports;
+      HEAPU32[((pnum)>>2)] = num;
+      return 0;
+    }
+  },
+}).then(function receiveInstantiationResult(result) {
+  wasmExports = result.instance.exports;
 
-    var b = wasmExports.memory.buffer;
-    HEAP8 = new Int8Array(b);
-    HEAPU8 = new Uint8Array(b);
-    HEAPU32 = new Uint32Array(b);
+  var b = wasmExports.memory.buffer;
+  HEAP8 = new Int8Array(b);
+  HEAPU8 = new Uint8Array(b);
+  HEAPU32 = new Uint32Array(b);
 
-    wasmExports.emscripten_stack_init();
+  wasmExports.emscripten_stack_init();
+}));
 
-    resolveInitialized();
-  });
-});
+let promiseInitialized = initialize();
