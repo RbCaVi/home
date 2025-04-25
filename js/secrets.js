ef17d5936a30a515d2db0582f9060e9813fbbd41
RbCaVi <robert@vail-family.com>
2025-04-25 17:23:51 -0400

    make secrets.js into a "namespace"

--- js/secrets.js
+++ js/secrets.js
@@ -1,29 +1,28 @@
-let secrets;
+window.secrets = (() => {
+  let secrets;
 
-async function secrets_init() {
-	secrets = await ((await fetch('./secrets.json')).json());
-}
+  async function init() {
+    secrets = await ((await fetch('./secrets.json')).json());
+  }
 
-const secretpromise = secrets_init();
+  const secretpromise = (() => secrets = await ((await fetch('./secrets.json')).json()));
 
-async function secrets_get(item, key) {
- if (secrets == undefined) {
-  await secretpromise;
- }
-	const [type, encrypted] = secrets[item];
-	switch (type) {
-	case 'text':
-		return crypt.decrypt(encrypted, key);
-		break;
-	case 'data':
-		return crypt.decryptdata(encrypted, key);
-		break;
-	default:
-		throw '???';
-	}
-}
-
-// take that
-function datatourl(data) {
-	return URL.createObjectURL(new Blob([Int8Array.from(data).buffer]));
-}
+  return {
+    get: (item, key) => {
+      if (secrets == undefined) {
+        await secretpromise;
+      }
+      const [type, encrypted] = secrets[item];
+      switch (type) {
+      case 'text':
+        return crypt.decrypt(encrypted, key);
+        break;
+      case 'data':
+        return crypt.decryptdata(encrypted, key);
+        break;
+      default:
+        throw '???';
+      }
+    },
+  };
+})();
