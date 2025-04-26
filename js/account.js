c474589476451b257878d0cebca361188b142e50
RbCaVi <robert@vail-family.com>
2025-04-26 10:41:21 -0400

    fixes to the new scripts

--- js/account.js
+++ js/account.js
@@ -1,11 +1,12 @@
 window.account = (() => {
-  const accountprototype = {
-    getid: async () => this.id ?? (this.id = await db.select('users', {username: "eq." + user.replace('"', '\\"'), select: "id"})),
-  }
+  const accountprototype = {};
   
   return {
     create: async (user, pass) => (await auth.makeaccount(user, pass), account.login(user, pass)),
-    login: async (user, pass) => Object.create(accountprototype, {token: {value: await auth.login(user, pass)}}),
+    login: async (user, pass) => Object.create(accountprototype, {
+      token: {value: await auth.login(user, pass)},
+      id: {value: (await db.select('users', {username: "eq." + user.replace('"', '\\"'), select: "id"}))[0].id},
+    }),
     addmethod: (name, f) => accountprototype[name] = f,
   };
 })();
--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,10 @@ account.addmethod("addscript", function addscript(name, script) {
   return db.call('add_script', {token: this.token, name, script});
 });
 
-account.addmethod("getownscripts", async function addscript(name, script) {
+account.addmethod("getownscripts", async function getownscripts() {
   return (await db.select(
     'scripts',
-    {user: `eq.${this.getid()}`, select: "created_at,name,script"}
+    {user: `eq.${this.id}`, select: "created_at,name,script"}
   )).map(({created_at, name, script}) => {
     return {
       timestamp: created_at,
@@ -16,7 +16,7 @@ account.addmethod("getownscripts", async function addscript(name, script) {
 });
 
 window.scripts = {
-  getscripts: async function getscripts(name, script) {
+  getscripts: async function getscripts() {
     return (await db.select(
       'scripts',
       {select: "created_at,name,script,users(username)"}
