ed15919b789a91d2eb18e5f6a8c911e0ed730db2
RbCaViTerminal <robert@robertvail.info>
2025-10-17 17:13:48 -0400

    MAGENTA::webrtc: all the functions (i think)

--- static/js/connect.js
+++ static/js/connect.js
@@ -5,10 +5,10 @@
 window.connect = (() => {
   const negotiateprototype = {
   	_connectionnegotiate: function(kind, data) {return db.call('negotiate_message', {token: this.owner.token, negotiation: this.id, kind, data});},
-  	sendinitiatordescription: function(data) {this._connectionnegotiate('idesc', data)},
-  	sendresponderdescription: function(data) {this._connectionnegotiate('rdesc', data)},
-  	sendinitiatorice: function(data) {this._connectionnegotiate('iice', data)},
-  	sendresponderice: function(data) {this._connectionnegotiate('rice', data)},
+  	sendinitiatordescription: function(data) {return this._connectionnegotiate('idesc', data)},
+  	sendresponderdescription: function(data) {return this._connectionnegotiate('rdesc', data)},
+  	sendinitiatorice: function(data) {return this._connectionnegotiate('iice', data)},
+  	sendresponderice: function(data) {return this._connectionnegotiate('rice', data)},
   	_getnegotiations: async function(kind) {
 		  return (await db.select(
 		    'connectionnegotiationmessages',
@@ -17,16 +17,33 @@ window.connect = (() => {
 		    return data;
 		  });
 		},
-		getinitiatordescription: function() {this._getnegotiations('idesc')[0]},
-  	getresponderdescription: function() {this._getnegotiations('rdesc')[0]},
-  	getinitiatorices: function() {this._getnegotiations('iice')},
-  	getresponderices: function() {this._getnegotiations('rice')},
+		getinitiatordescriptions: function() {return this._getnegotiations('idesc')},
+  	getresponderdescriptions: function() {return this._getnegotiations('rdesc')},
+  	getinitiatorices: function() {return this._getnegotiations('iice')},
+  	getresponderices: function() {return this._getnegotiations('rice')},
   };
 
-  const offerprototype = {};
+  const offerprototype = {
+  	getnegotiations: async function() {
+		  return (await db.select(
+		    'connectionnegotiation',
+		    {select: "id,created_at,connectionoffers(info,users(username)),users(username)", offer: 'eq.' + this.id}
+		  )).map(({id, created_at, connectionoffers: {info, users: {username: initiator}}, users: {username: responder}}) => {
+		    return Object.create(negotiateprototype, {
+		    	owner: {value: this.owner},
+		    	id: {value: id},
+		      timestamp: {value: created_at},
+					info: {value: info},
+					initiator: {value: initiator},
+					responder: {value: responder},
+		    });
+		  });
+		},
+  };
 
 	account.addmethod('createoffer', async function(info) {
 		return Object.create(offerprototype, {
+			owner: {value: this},
 			id: {value: (await db.call('create_offer', {token: this.token, info}))},
 			info: {value: info},
 		});
@@ -45,22 +62,7 @@ window.connect = (() => {
 				initiator: {value: initiator},
 				responder: {value: responder},
 	    });
-	  });
-	});
-	account.addmethod('getacceptedoffers', async function(id) {
-	  return (await db.select(
-	    'connectionnegotiation',
-	    {select: "id,created_at,connectionoffers(info,users(username)),users(username)", offer: 'eq.' + id}
-	  )).map(({id, created_at, connectionoffers: {info, users: {username: initiator}}, users: {username: responder}}) => {
-	    return Object.create(negotiateprototype, {
-	    	owner: {value: this},
-	    	id: {value: id},
-	      timestamp: {value: created_at},
-				info: {value: info},
-				initiator: {value: initiator},
-				responder: {value: responder},
-	    });
-	  });
+	  })[0];
 	});
 
 	return {
